3bb2e7bdffd53bf94de5725c0fb3c956306de421
LEVI <rezkiandra4123@gmail.com>
2024-02-13 13:47:24 +0700

    migrate from vite to webpack

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,35 +6,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <link rel="shortcut icon" type="image/png" href="{{ asset('modernize/src/assets/images/logos/favicon.png') }}" />
-  <link rel="stylesheet" href="{{ asset('/modernize/src/assets/css/styles.min.css') }}" />
-  <link rel="stylesheet" href="{{ asset('modernize/src/assets/css/icons/tabler-icons/tabler-icons.css') }}">
-
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <title>@yield('title') - Warung Digital</title>
 </head>
 
 <body>
-  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed">
-
-    {{-- @include('components.sidebar') --}}
-
-    <div class="body-wrapper">
-      {{-- @include('components.navbar') --}}
-
-      <div class="container-fluid">
-        @yield('content')
-        {{-- @include('components.footer') --}}
-      </div>
-
-    </div>
-  </div>
-
-  <script src="{{ asset('modernize/src/assets/libs/jquery/dist/jquery.min.js') }}"></script>
-  <script src="{{ asset('modernize/src/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('modernize/src/assets/js/sidebarmenu.js') }}"></script>
-  <script src="{{ asset('modernize/src/assets/js/app.min.js') }}"></script>
-  <script src="{{ asset('modernize/src/assets/libs/simplebar/dist/simplebar.js') }}"></script>
-
+  @yield('content')
+	
+  <script src="{{ mix('js/app.js') }}" defer></script>
 </body>
 
 </html>
