4a6ce4633047e516dba6a737fb577887852f909b
Diki_Aryangga <dikiaryangga27@gmail.com>
2024-05-03 21:12:40 +0700

    remove unused files

--- storage/framework/views/27bb1815ab51f08d6f32bf3b4ba4c2127b68c7c2.php
+++ storage/framework/views/27bb1815ab51f08d6f32bf3b4ba4c2127b68c7c2.php
@@ -8,21 +8,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&ampdisplay=swap"
-      rel="stylesheet" />
-
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&ampdisplay=swap" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link rel="icon" type="image/x-icon" href="<?php echo e(asset('materio/assets/img/favicon/favicon.ico')); ?>" />
     <link rel="stylesheet" href="<?php echo e(asset('materio/assets/vendor/fonts/materialdesignicons.css')); ?>" />
     <link rel="stylesheet" href="<?php echo e(asset('materio/assets/vendor/libs/node-waves/node-waves.css')); ?>" />
-    <link rel="stylesheet" href="<?php echo e(asset('materio/assets/vendor/css/core.css')); ?>"
-      class="template-customizer-core-css" />
-    <link rel="stylesheet" href="<?php echo e(asset('materio/assets/vendor/css/theme-default.css')); ?>"
-      class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="<?php echo e(asset('materio/assets/vendor/css/core.css')); ?>" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="<?php echo e(asset('materio/assets/vendor/css/theme-default.css')); ?>" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="<?php echo e(asset('materio/assets/css/demo.css')); ?>" />
     <link rel="stylesheet" href="<?php echo e(asset('materio/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')); ?>" />
-    <link rel="stylesheet" href="<?php echo e(asset('materio/assets/vendor/libs/apex-charts/apex-charts.css')); ?>" />
 
     <script src="<?php echo e(asset('materio/assets/vendor/js/helpers.js')); ?>"></script>
     <script src="<?php echo e(asset('materio/assets/js/config.js')); ?>"></script>
@@ -70,9 +65,9 @@
     <script src="<?php echo e(asset('materio/assets/vendor/libs/node-waves/node-waves.js')); ?>"></script>
     <script src="<?php echo e(asset('materio/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')); ?>"></script>
     <script src="<?php echo e(asset('materio/assets/vendor/js/menu.js')); ?>"></script>
-    <script src="<?php echo e(asset('materio/assets/vendor/libs/apex-charts/apexcharts.js')); ?>"></script>
     <script src="<?php echo e(asset('materio/assets/js/main.js')); ?>"></script>
-    <script src="<?php echo e(asset('materio/assets/js/dashboards-analytics.js')); ?>"></script>
+    
+    
     <?php echo $__env->yieldPushContent('scripts'); ?>
   </body>
 
