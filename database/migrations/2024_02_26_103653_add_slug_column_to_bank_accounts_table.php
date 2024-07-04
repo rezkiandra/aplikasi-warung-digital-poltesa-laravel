1c0391ced3dbebc760a0854aa0d45588544e3539
GUEST-HP <dikiaryangga27@gmail.com>
2024-07-04 16:28:57 +0700

    copy: copy migrations from other to main

---
+++ database/migrations/2024_02_26_103653_add_slug_column_to_bank_accounts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSlugColumnToBankAccountsTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('bank_accounts', function (Blueprint $table) {
+      $table->string('slug')->after('bank_name');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('bank_accounts', function (Blueprint $table) {
+      $table->dropColumn('slug');
+    });
+  }
+}
---
+++ database/migrations/2024_03_04_213841_add_status_column_to_sellers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusColumnToSellersTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('sellers', function (Blueprint $table) {
+      $table->enum('status', ['active', 'inactive', 'pending'])->after('gender')->default('pending');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('sellers', function (Blueprint $table) {
+      $table->dropColumn('status');
+    });
+  }
+}
